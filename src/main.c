f768c4945f2e43a93500d6136394b4e88176455a
Fabio Battaglia <fbattaglia@mockbook.local>
2019-12-10 21:43:53 +0100

    Add empty project

---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include <stdint.h>
+#include <avr/io.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+#include "main.h"
+
+
+int main(void) {
+	while(1);
+
+    return 0;
+}
+
+
