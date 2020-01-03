6cf6d8625cbbf176ae711039c08a0d15f88c91fb
Baglio Tabifata <tabaglio@posteo.net>
2020-01-03 10:05:20 +0100

    Add support for the middle mouse button

--- src/libs/ps22ser/ps22ser.c
+++ src/libs/ps22ser/ps22ser.c
@@ -1,15 +1,27 @@
 #include "ps22ser.h"
 
-uint8_t ps2bufToSer(uint8_t *src, uint8_t *dst) {
+uint8_t ps2bufToSer(const uint8_t *src, uint8_t *dst, uint8_t *state) {
     if(!(src[0] & 0x08)) return 0; // The only validation we can do, checking the single fixed bit in the first byte.
+    uint8_t retval = 0x01;
 
     // Prepare the destination
     dst[0] = 0xC0;
-    dst[1] = dst[2] = 0x80; // Clear the destination. Leave the first bit to 1 to simulate 7n2 (we're actually using 8n1)
+    dst[1] = dst[2] = dst[3] = 0x80; // Clear the destination. Leave the first bit to 1 to simulate 7n2 (we're actually using 8n1)
 
     // Set the button status, ignore the middle one, for now
     dst[0] |= (src[0] & 0x01) << 5; // Left
     dst[0] |= (src[0] & 0x02) << 3; // Right
+
+    // Now handle the middle one
+    uint8_t midb = src[0] & 0x04; // Middle button state
+    if(midb) {
+        retval |= 0x02; // We're going to use the 4th byte
+        *state |= 0x01; // Save the state regarding the middle button state
+    } else if (*state & 0x01) { // The button is not pressed, but it was before
+        retval |= 0x02; // We're going to use the 4th byte
+        *state &= 0xFE;
+    }
+    dst[3] |= midb << 3; // Set the value of the 4th byte
 
     // PS/2 has 9-bit two's complement notation
     // Serial (microsoft) has 8-bit two's complement notation
@@ -40,5 +52,5 @@ uint8_t ps2bufToSer(uint8_t *src, uint8_t *dst) {
 
     // Let's ignore the overflow indicators for now
 
-    return 1;
+    return retval;
 }
