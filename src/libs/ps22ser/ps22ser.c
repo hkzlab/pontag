658a778bc2afeb7eec3fb5d24ad0837e5cc20eb3
tabaglio <tabaglio@posteo.net>
2020-01-11 16:17:43 +0100

    Fix proto

--- src/libs/ps22ser/ps22ser.c
+++ src/libs/ps22ser/ps22ser.c
@@ -28,27 +28,18 @@ uint8_t ps2bufToSer(const uint8_t *src, uint8_t *dst, uint8_t *state) {
     // We can ignore the least significant bit or...
     // ignore the most significant one.
 
-    // TEST 1 - Ignore the least significant bit
-    // Movement - First byte
-    //dst[0] |= (src[0] & 0x20) >> 2; // Y7
-    //dst[0] |= (src[0] & 0x10) >> 3; // X7
-    //dst[0] |= (src[2] & 0x80) >> 5; // Y6
-    //dst[0] |= (src[1] & 0x80) >> 7; // X6
-    // Movement - Second byte
-    //dst[1] |= (src[1] & 0x7E) >> 1; // X5-X0
-    // Movement - Third byte
-    //dst[2] |= (src[2] & 0x7E) >> 1; // Y6-Y0
+    // We found out that this works best - Ignore the most significant bit
+    // We need to invert Y
+    int8_t y_mov = -((src[0] & 0x20) ? (0x80 | src[2]) : src[2]);
 
-    // TEST 2 - Ignore the most significant bit
     // Movement - First byte
-    dst[0] |= (src[2] & 0x40) >> 4; // Y6
     dst[0] |= (src[1] & 0x40) >> 6; // X6
-    dst[0] |= ((src[0] & 0x20) ? 0x08 : 0x00); // Y7 - Sign
+    dst[0] |= (y_mov >> 4) & 0x0C;
     dst[0] |= ((src[0] & 0x10) ? 0x02 : 0x00); // X7 - Sign
     // Movement - Second byte
     dst[1] |= (src[1] & 0x3F) >> 0; // X5-X0
     // Movement - Third byte
-    dst[2] |= (src[2] & 0x3F) >> 0; // Y6-Y0
+    dst[2] |= (y_mov & 0x3F) >> 0; // Y5-Y0
 
     // Let's ignore the overflow indicators for now
 
