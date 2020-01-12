de2336e341add2f39ba17521d148819a61385903
Baglio Tabifata <tabaglio@posteo.net>
2020-01-12 11:34:39 +0100

    Add datasheet and add comments to main code

--- src/main.c
+++ src/main.c
@@ -28,20 +28,51 @@ void setup_detection_interrupt(void);
 
 int main(void) {
     uint8_t serial_pkt_buf[4]; // Buffer for serial packets
-    uint8_t converter_status; // Converter status
-    uint8_t converter_result;
+    uint8_t converter_status; // PS/2 -> Serial conversion state var, used to keep track of state during iteration
+    uint8_t converter_result; // Instanteneous result of the conversion
+
+    /**
+     * We will do the following
+     * 1 - Set all unused I/O as input with pull-up enabled
+     * 2 - Setup the pins for the serial port: TX output, RX input (without pullup), RTS (INT1) as input, without pullup
+     * 3 - Setup the pins for PS/2 communication: CLOCK input with pullup, DATA input with pullup (will be managed by PS/2 code)
+     * 
+     * ATMega328p
+     * - UART RX : PD0
+     * - UART TX : PD1
+     * - UART RTS : PD3
+     * - PS/2 CLOCK : PD2
+     * - PS/2 DATA : PB1
+     * 
+     * ATMega 8a
+     * - UART RX : PD0
+     * - UART TX : PD1
+     * - UART RTS : PD3
+     * - PS/2 CLOCK : PD2
+     * - PS/2 DATA : PB1
+     * 
+     * ATTiny4313
+     * - UART RX : PD0
+     * - UART TX : PD1
+     * - UART RTS : PD3
+     * - PS/2 CLOCK : PD2
+     * - PS/2 DATA : PB1
+     * 
+     */
 
     // Set the pull-up resistor to all unused I/O ...
     DDRB &= 0x03; // PB2-7 as input...
-    PORTB |= 0xFC; // ...and with pullup high
+    PORTB |= 0xFC; // ...and with pull-up
 
+#if defined (__AVR_ATmega328P__) || defined (__AVR_ATmega8A__)
     DDRC &= 0xC0; // PC0-5 as input...
-    PORTC |= 0x3F; // ...and with pull-up high
+    PORTC |= 0x3F; // ...and with pull-up
+#endif
 
-    DDRD &= 0x06; // Make sure PD0,PD3-7 are input
-    PORTD |= 0xF0; // Enable pullup resistors on PD4-7
+    DDRD &= 0x06; // PD0,PD3-7 as input...
+    PORTD |= 0xF0; // ...but enable pullup only on PD4-7
 
-    // Initialize serial port for output
+    // Initialize serial port
     uart_init();
     stdout = &uart_output;
     stdin  = &uart_input;
@@ -80,10 +111,13 @@ int main(void) {
                 buf_counter = cur_counter;
                 converter_result = ps2bufToSer(ps2_buf, serial_pkt_buf, &converter_status);
 
-                uart_putchar(serial_pkt_buf[0], NULL);
-                uart_putchar(serial_pkt_buf[1], NULL);
-                uart_putchar(serial_pkt_buf[2], NULL);
-
+                if(converter_result) {
+                    // Transmit the converted data to the serial port
+                    uart_putchar(serial_pkt_buf[0], NULL);
+                    uart_putchar(serial_pkt_buf[1], NULL);
+                    uart_putchar(serial_pkt_buf[2], NULL);
+                    if(converter_result & 0x02) uart_putchar(serial_pkt_buf[3], NULL); // Send the fourth byte, according to the Logitech serial protocol
+                }
                 //fprintf(stdout, "%.2X %.2X %.2X %.2X - %.2X\n", serial_pkt_buf[0], serial_pkt_buf[1], serial_pkt_buf[2], serial_pkt_buf[3], converter_result);
             }
         }
@@ -93,17 +127,20 @@ int main(void) {
 }
 
 void setup_detection_interrupt(void) {
-    // Enable INT1
+    // Enable INT1, and have it toggle at any logical level change
 #if defined (__AVR_ATmega128__) || defined (__AVR_ATmega328P__)
     // Toggle when the logical level changes
     EICRA |= (1 << ISC10);
     EICRA &= ~(1 << ISC11);
     EIMSK |= (1 << INT1);
-    // TODO: Implement for the other MCU types
 #elif defined (__AVR_ATtiny4313__)
+    MCUCR |= (1 << ISC10);
+    MCUCR &= ~(1 << ISC11);
     GIMSK |= (1 << INT1);
 #elif defined (__AVR_ATmega8A__)
-    GICR  |= (1 << INT1); // Enable INT1
+    MCUCR |= (1 << ISC10);
+    MCUCR &= ~(1 << ISC11)
+    GICR  |= (1 << INT1);
 #endif
 }
 
