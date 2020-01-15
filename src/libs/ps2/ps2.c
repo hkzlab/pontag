97155570bab16e67c2270bde221afde400d1edb7
Fabio Battaglia <hkzlabnet@gmail.com>
2020-01-15 09:55:56 +0100

    Disable code that doesn't run on 328p

--- src/libs/ps2/ps2.c
+++ src/libs/ps2/ps2.c
@@ -1,7 +1,7 @@
 // PS/2 protocol implementation
 //
 // This implementation is entirely interrupt-driven so all comms happens in background.
-// Clock is tied to INT0 pin and events are handled in INT0 ISR handler. 
+// Clock is tied to INT0 pin and events are handled in INT0 ISR handler.
 //
 // Events not triggered by clock (end of transmission, transmission request, watchdog,
 // error recovery) use Timer0. Watch out how state changes in different handlers.
@@ -46,50 +46,56 @@ enum _state {
     RX_DATA,            // Receiving data
     RX_PARITY,          // Receiving parity bit
     RX_STOP,            // Receiving stop bit
-   
+
     TX_REQ0,            // Requesting to send
     TX_DATA,            // Transmitting data
     TX_PARITY,          // Transmitting parity bit
     TX_STOP,            // Transmitting stop bit
     TX_ACK,             // Waiting ACK
     TX_END,             // Waiting for TX end
-    
+
     ERROR = 255         // Error state
 };
 
-void ps2_dir(uint8_t,uint8_t);      // Set busses direction (1 == in)
-void ps2_clk(uint8_t);              // Set clk
-void ps2_dat(uint8_t);              // Set dat
+void ps2_dir(uint8_t dat_in,uint8_t clk_in);      // Set buses direction (1 == in)
+void ps2_clk(uint8_t c);              // Set clk
+void ps2_dat(uint8_t d);              // Set dat
 
-uint8_t ps2_busy(void) { return state != IDLE; }
+uint8_t ps2_busy(void) {
+    return state != IDLE;
+}
 
 void ps2_init(void) {
     state = IDLE;
     rx_head = 0;
     rx_tail = 0;
     ps2_enable_recv(0);
-  
+
     // Toggle INT0 at the falling edge
-#if defined (__AVR_ATmega328P__)
-    EICRA |= _BV(ISC10);
-#elif defined (__AVR_ATtiny4313__) || defined (__AVR_ATmega8A__)
+#if defined (__AVR_ATmega8A__)
     MCUCR |= _BV(ISC01);
+#elif defined (__AVR_ATmega328P__)
+    EICRA |= _BV(ISC10);
 #endif
 
     // Disable the timer 0 interrupts
+#if defined (__AVR_ATmega8A__)
     TIMSK &= ~_BV(TOIE0);
+#elif defined (__AVR_ATmega328P__)
+    // TODO
+#endif
 }
 
 /// Begin error recovery: disable reception and wait for timer interrupt
 void ps2_recover(void) {
     if (state == ERROR) {
         ps2_enable_recv(0);
-#if defined (__AVR_ATmega8A__)
-	// This is developed for 8Mhz
+#if defined (__AVR_ATmega8A__) // 8 Mhz
         TCNT0 = 255-35; // approx 1ms
         TIMSK |= _BV(TOIE0);
         TCCR0 = 4;  // enable: clk/256
 #elif defined (__AVR_ATmega328P__)
+        // TODO
 #endif
     }
 }
@@ -98,12 +104,20 @@ void ps2_enable_recv(uint8_t enable) {
     if (enable) {
         state = IDLE;
         ps2_dir(1,1);
-        // enable INT0 interruptt
+        // enable INT0 interrupt
+#if defined (__AVR_ATmega8A__)
         GIFR |= _BV(INTF0);
         GICR |= _BV(INT0);
+#elif defined (__AVR_ATmega328P__)
+        // TODO
+#endif
     } else {
         // disable INT0, then everything else
+#if defined (__AVR_ATmega8A__)
         GICR &= ~_BV(INT0);
+#elif defined (__AVR_ATmega328P__)
+        // TODO
+#endif
         ps2_clk(0);
         ps2_dir(1,0);
     }
@@ -124,118 +138,126 @@ void ps2_dat(uint8_t d) {
 
 uint8_t ps2_avail() {
     return rx_head != rx_tail;
-} 
+}
 
 uint8_t ps2_getbyte() {
-	uint8_t result = rx_buf[rx_tail];
-	rx_tail = (rx_tail + 1) % PS2_RXBUF_LEN;
-	
-	return result;
+    uint8_t result = rx_buf[rx_tail];
+    rx_tail = (rx_tail + 1) % PS2_RXBUF_LEN;
+
+    return result;
 }
 
 void ps2_sendbyte(uint8_t byte) {
     while (state != IDLE);
-     
+
     // 1. pull clk low for 100us
     ps2_enable_recv(0);
 
     tx_byte = byte;
     state = TX_REQ0;
-    
+
     // 128us
-    TCNT0 = 255-4; 
+#if defined (__AVR_ATmega8A__)
+    TCNT0 = 255-4;
     TIMSK |= _BV(TOIE0);
     TCCR0 = 4;
-    
+#elif defined (__AVR_ATmega328P__)
+    // TODO
+#endif
+
     while (state != IDLE);
 }
 
 /// Happens every negative PS2 clock transition.
 ///
 /// ISR_NOBLOCK because nothing here is really critical, while C1351 emulation
-/// is really time critical. 
+/// is really time critical.
 ISR(INT0_vect, ISR_NOBLOCK) {
     uint8_t ps2_indat = ps2_datin();
     switch (state) {
-        case ERROR:
-            break;  
-            
-            // Receive states
-                      
-        case IDLE:
-            if (ps2_indat == 0) {
-                state = RX_DATA;
-                bits = 8;
-                parity = 0;
-                recv_byte = 0;
-            } else {
-                state = ERROR;
-            }
-            break;
-        case RX_DATA:
-            recv_byte = (recv_byte >> 1) | ps2_indat;
-            parity ^= ps2_indat;
-            
-            if (--bits == 0) {
-                state = RX_PARITY;
-            }
-            break;
-        case RX_PARITY:
-            parity ^= ps2_indat;
-            if (parity) {
-                state = RX_STOP;
-            } else {
-                state = ERROR;
-            }
-            break;
-        case RX_STOP:
-            if (!ps2_indat) {
-                state = ERROR;
-            } else {
-                rx_buf[rx_head] = recv_byte;
-                rx_head = (rx_head + 1) % PS2_RXBUF_LEN;
-                
-                state = IDLE;                
-            }
-            break;
-            
-            // Transmit states
-            
-        case TX_REQ0:
-            // state will be switched in timer interrupt handler
-            break;
-        case TX_DATA:
-            ps2_dat(tx_byte & 001);
-            parity ^= tx_byte & 001;
-            tx_byte >>= 1;
-            if (--bits == 0) {
-                state = TX_PARITY;
-            }
-            break;
-        case TX_PARITY:
-            ps2_dat(parity ^ 001);
-            state = TX_STOP;            
-            break;
-        case TX_STOP:
-            ps2_dat(0);
-            ps2_dir(1,1);
-            state = TX_ACK;         
-            break;
-        case TX_ACK:
-            if (ps2_indat) {
-                state = ERROR;
-            } else {
-                // this will end in TMR0 interrupt
-                state = TX_END;                
+    case ERROR:
+        break;
 
-                waitcnt = 50;           // after 100us it's an error
-                TIMSK |= _BV(TOIE0);    // enable TMR0 interrupt
-                TCNT0 = 255-2;              // 4 counts: 2us
-                TCCR0 = 2;              // prescaler = f/8: go!
-            }
-            break;
-        case TX_END:
-            break;
+    // Receive states
+
+    case IDLE:
+        if (ps2_indat == 0) {
+            state = RX_DATA;
+            bits = 8;
+            parity = 0;
+            recv_byte = 0;
+        } else {
+            state = ERROR;
+        }
+        break;
+    case RX_DATA:
+        recv_byte = (recv_byte >> 1) | ps2_indat;
+        parity ^= ps2_indat;
+
+        if (--bits == 0) {
+            state = RX_PARITY;
+        }
+        break;
+    case RX_PARITY:
+        parity ^= ps2_indat;
+        if (parity) {
+            state = RX_STOP;
+        } else {
+            state = ERROR;
+        }
+        break;
+    case RX_STOP:
+        if (!ps2_indat) {
+            state = ERROR;
+        } else {
+            rx_buf[rx_head] = recv_byte;
+            rx_head = (rx_head + 1) % PS2_RXBUF_LEN;
+
+            state = IDLE;
+        }
+        break;
+
+    // Transmit states
+
+    case TX_REQ0:
+        // state will be switched in timer interrupt handler
+        break;
+    case TX_DATA:
+        ps2_dat(tx_byte & 001);
+        parity ^= tx_byte & 001;
+        tx_byte >>= 1;
+        if (--bits == 0) {
+            state = TX_PARITY;
+        }
+        break;
+    case TX_PARITY:
+        ps2_dat(parity ^ 001);
+        state = TX_STOP;
+        break;
+    case TX_STOP:
+        ps2_dat(0);
+        ps2_dir(1,1);
+        state = TX_ACK;
+        break;
+    case TX_ACK:
+        if (ps2_indat) {
+            state = ERROR;
+        } else {
+            // this will end in TMR0 interrupt
+            state = TX_END;
+
+#if defined (__AVR_ATmega8A__)
+            waitcnt = 50;           // after 100us it's an error
+            TIMSK |= _BV(TOIE0);    // enable TMR0 interrupt
+            TCNT0 = 255-2;              // 4 counts: 2us
+            TCCR0 = 2;              // prescaler = f/8: go!
+#elif defined (__AVR_ATmega328P__)
+            // TODO
+#endif
+        }
+        break;
+    case TX_END:
+        break;
     }
     ps2_recover();
 }
@@ -243,67 +265,81 @@ ISR(INT0_vect, ISR_NOBLOCK) {
 /// transmit timer and error recovery vector
 ISR(TIMER0_OVF_vect) {
     static uint8_t barkcnt = 0;
-    
+
     switch (state) {
-        case ERROR:
-            state = IDLE;
-            ps2_clk(0);
-            ps2_dat(0);
-            ps2_enable_recv(1);
-            
-            // stop timer
+    case ERROR:
+        state = IDLE;
+        ps2_clk(0);
+        ps2_dat(0);
+        ps2_enable_recv(1);
+
+        // stop timer
+#if defined (__AVR_ATmega8A__)
+        TIMSK &= ~_BV(TOIE0);
+        TCCR0 = 0;
+#elif defined (__AVR_ATmega328P__)
+        // TODO
+#endif
+        break;
+    case TX_REQ0:
+        // load the timer to serve as a watchdog
+        // after 20 barks this is an error
+#if defined (__AVR_ATmega8A__)
+        barkcnt = 20;
+        TIMSK |= _BV(TOIE0);    // enable TMR0 interrupt
+        TCNT0 = 0;//255;            // 20*255*256/8e6 == 163ms
+        TCCR0 = 4;               // prescaler = /256, go!
+#elif defined (__AVR_ATmega328P__)
+        // TODO
+#endif
+        // waited for 100us after pulling clock low, pull data low
+        ps2_dat(0);
+        ps2_dir(0,0);
+
+        // release the clock line
+        ps2_dir(0,1);
+
+#if defined (__AVR_ATmega8A__)
+        GIFR |= _BV(INTF0); // clear INT0 flag
+        GICR |= _BV(INT0);  // enable INT0 @(negedge clk)
+#elif defined (__AVR_ATmega328P__)
+        // TODO
+#endif
+
+        // see you in INT0 handler
+        bits = 8;
+        parity = 0;
+
+        state = TX_DATA;
+        break;
+    case TX_END:
+        // wait until both clk and dat are up, that will be all
+        if (ps2_clkin() && ps2_datin()) {
+#if defined (__AVR_ATmega8A__)
             TIMSK &= ~_BV(TOIE0);
             TCCR0 = 0;
-            break;
-        case TX_REQ0:
-            // load the timer to serve as a watchdog
-            // after 20 barks this is an error
-            barkcnt = 20;
-            TIMSK |= _BV(TOIE0);    // enable TMR0 interrupt
-            TCNT0 = 0;//255;            // 20*255*256/8e6 == 163ms
-            TCCR0 = 4;               // prescaler = /256, go!
-
-            // waited for 100us after pulling clock low, pull data low
-            ps2_dat(0);
-            ps2_dir(0,0);
-            
-            // release the clock line
-            ps2_dir(0,1); 
-            
-            GIFR |= _BV(INTF0); // clear INT0 flag
-            GICR |= _BV(INT0);  // enable INT0 @(negedge clk)
-                        
-            // see you in INT0 handler
-            bits = 8;
-            parity = 0;
-
-            state = TX_DATA;
-            break;
-        case TX_END:
-            // wait until both clk and dat are up, that will be all
-            if (ps2_clkin() && ps2_datin()) {
-                TIMSK &= ~_BV(TOIE0);
-                TCCR0 = 0;
-                state = IDLE;
-            } else {
-                if (waitcnt == 0) {
-                    state = ERROR;
-                    ps2_recover();
-                } else {
-                    waitcnt--;
-                }
-            }
-            break;
-        default:
-            // watchdog barked: probably not a mouse!
-            if (barkcnt == 0) {
+#elif defined (__AVR_ATmega328P__)
+            // TODO
+#endif
+            state = IDLE;
+        } else {
+            if (waitcnt == 0) {
                 state = ERROR;
                 ps2_recover();
             } else {
-                barkcnt--;
+                waitcnt--;
             }
-            break;
+        }
+        break;
+    default:
+        // watchdog barked: probably not a mouse!
+        if (barkcnt == 0) {
+            state = ERROR;
+            ps2_recover();
+        } else {
+            barkcnt--;
+        }
+        break;
     }
 }
 
-//$Id$
